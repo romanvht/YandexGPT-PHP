404026ea5b5e32dc1216222defcf7584d031b1b9
Roman Kostin <36431458+romanvht@users.noreply.github.com>
2024-03-01 16:10:19 +1000

    Update yachat.php

--- yachat.php
+++ yachat.php
@@ -1,119 +1,127 @@
 <?php
 namespace yachat;
 
-class YaChat{
-    protected const CLIENT_AUTH = "<AUTH TOKEN>";
-    protected const X_FOLDER_ID = "<FOLDER ID>";
+class YaChat
+{
+  protected const CLIENT_AUTH = "<AUTH TOKEN>";
+  protected const X_FOLDER_ID = "<FOLDER ID>";
 
-    protected static $instance; 
-    private static $token = false;
-    private static $tokenExp = false;
-    private static $messages = [];
+  protected static $instance;
+  private static $token = false;
+  private static $tokenExp = false;
+  private static $messages = [];
 
-    public static function getInstance(){
-        if (is_null(self::$instance)) {
-            self::$instance = new self;
-            self::$token = file_get_contents('token_ya.txt');
-            self::$tokenExp = file_get_contents('token_ya_ext.txt');
-        }
-        return self::$instance;
+  public static function getInstance()
+  {
+    if (is_null(self::$instance)) {
+      self::$instance = new self;
+      self::$token = file_get_contents('token_ya.txt');
+      self::$tokenExp = file_get_contents('token_ya_ext.txt');
+    }
+    return self::$instance;
+  }
+
+  public static function getHistory()
+  {
+    return self::$messages;
+  }
+  public static function clearHistory()
+  {
+    self::$messages = [];
+  }
+  public static function updateHistory($messages)
+  {
+    self::$messages = $messages;
+  }
+
+  private static function get($url, $headers, $data)
+  {
+    $curl = curl_init();
+    curl_setopt_array($curl, [
+      CURLOPT_URL => $url,
+      CURLOPT_SSL_VERIFYPEER => 0,
+      CURLOPT_POST => 1,
+      CURLOPT_HTTPHEADER => $headers,
+      CURLOPT_RETURNTRANSFER => 1,
+    ]);
+    if (!empty($data)) {
+      curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
     }
 
-    public static function getHistory(){
-       return self::$messages;
-    }
-    public static function clearHistory(){
-       self::$messages = [];
-    }
-    public static function updateHistory($messages){
-       self::$messages = $messages;
-    }
-    
-   private static function get($url, $headers, $data){
-      $curl = curl_init();         
-        curl_setopt_array($curl, [
-            CURLOPT_URL => $url,
-            CURLOPT_SSL_VERIFYPEER => 0,
-            CURLOPT_POST => 1,
-            CURLOPT_HTTPHEADER => $headers,
-            CURLOPT_RETURNTRANSFER => 1,
-        ]);
-        if(!empty($data)){           
-            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);              
-        }
 
+    $result = curl_exec($curl);
+    return json_decode($result, true);
+  }
 
-        $result = curl_exec($curl);         
-        return json_decode($result,true);
-   }
+  public static function getToken($force = false)
+  {
+    if (!self::$token || !self::$tokenExp || self::$tokenExp < time() || $force === true) {
+      $url = "https://iam.api.cloud.yandex.net/iam/v1/tokens";
+      $data = '{"yandexPassportOauthToken":"' . self::CLIENT_AUTH . '"}';
+      $headers = [
+        'Content-Type: application/json',
+        'Content-Length: ' . strlen($data)
+      ];
+      $result = self::get($url, $headers, $data);
 
-    public static function getToken($force = false){           
-       if(!self::$token || !self::$tokenExp || self::$tokenExp < time() || $force === true){
-          $url = "https://iam.api.cloud.yandex.net/iam/v1/tokens";
-	  $data = '{"yandexPassportOauthToken":"'.self::CLIENT_AUTH.'"}';
-          $headers = [
-             	'Content-Type: application/json',
-		'Content-Length: '.strlen($data)
+      if (!empty($result["iamToken"])) {
+        self::$token = $result["iamToken"];
+        file_put_contents('token_ya.txt', self::$token);
+        self::$tokenExp = strtotime($result["expiresAt"]);
+        file_put_contents('token_ya_ext.txt', self::$tokenExp);
+      } else {
+        self::$token = false;
+        file_put_contents('token_ya.txt', '');
+        self::$tokenExp = false;
+        file_put_contents('token_ya_ext.txt', '');
+      }
+    }
+    return self::$token;
+  }
+
+  public static function answer($question, $temperature = 0.6)
+  {
+    $answer = "";
+    if (!empty($question)) {
+      $tok = self::getToken();
+
+      if ($tok) {
+        $url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion";
+        $headers = [
+          'Content-Type: application/json',
+          'Authorization: Bearer ' . $tok,
+          'x-folder-id: ' . self::X_FOLDER_ID
+        ];
+        $messages = self::$messages;
+        $messages[] = [
+          "role" => "user",
+          "text" => $question
+        ];
+        $data = [
+          "modelUri" => "gpt://" . self::X_FOLDER_ID . "/yandexgpt-lite/latest",
+          "completionOptions" => [
+            "stream" => false,
+            "temperature" => $temperature,
+            "max_tokens" => 2000,
+          ],
+          "messages" => $messages
+        ];
+        $result = self::get($url, $headers, json_encode($data));
+
+        $answer = $result['result']['alternatives'][0]['message']['text'];
+
+        if (!empty($answer)) {
+          $messages[] = [
+            "role" => "assistant",
+            "text" => $answer
           ];
-          $result = self::get($url, $headers, $data);
-          
-          if(!empty($result["iamToken"])){
-             self::$token = $result["iamToken"];
-	     file_put_contents('token_ya.txt', self::$token);
-             self::$tokenExp = strtotime($result["expiresAt"]);
-	     file_put_contents('token_ya_ext.txt', self::$tokenExp);		 
-          }else{
-             self::$token = false;
-             file_put_contents('token_ya.txt', '');
-             self::$tokenExp =  false;
-             file_put_contents('token_ya_ext.txt', '');               
-          }          
-       }
-       return self::$token;
+
+          self::$messages = $messages;
+        }
+      }
     }
-
-    public static function answer($question, $temperature = 0.6){
-       $answer = "";
-       if(!empty($question)){
-          $tok = self::getToken();          
-
-          if($tok){
-            $url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion";
-            $headers = [
-		'Content-Type: application/json',
-                'Authorization: Bearer ' .$tok,  
-		'x-folder-id: '.self::X_FOLDER_ID
-            ];
-			$messages = self::$messages;
-            $messages[] = [
-                "role" => "user",
-                "text"=> $question
-            ];
-            $data = [
-                "modelUri" => "gpt://".self::X_FOLDER_ID."/yandexgpt-lite/latest",
-		"completionOptions" => [
-			"stream" => false,
-			"temperature" => $temperature,
-			"max_tokens" => 2000,
-		],
-		"messages" => $messages
-             ];
-            $result = self::get($url, $headers, json_encode($data));            
-            
-            $answer = $result['result']['alternatives'][0]['message']['text'];
-			
-            if(!empty($answer)){
-		$messages[] = [
-			"role" => "assistant",
-			"text"=> $answer
-		];
-
-		self::$messages = $messages;
-            }
-          }
-       }   
-       return $answer;   
-    }
+    return $answer;
+  }
 }
 
 ?>
